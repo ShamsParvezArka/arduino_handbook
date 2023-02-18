eee73fdb0d2caaa8732d1ed25de2f2ca52df40a4
Shams Parvez Arka <parvez6826@gmail.com>
2023-02-19 00:46:02 +0600

    understanding someone:: to be continued ...

--- keypad.ino
+++ keypad.ino
@@ -8,7 +8,7 @@ char custom_key[ROW][COL] = {
 	{'4', '5', '6', 'B'},
 	{'7', '8', '9', 'C'},
 	{'*', '0', '#', 'D'},
-}
+};
 
 byte row_pins[ROW] = {13, 12, 11, 10};
 byte col_pins[COL] = {9,   8,  7,  6};
@@ -20,12 +20,17 @@ Keypad custom_keypad = Keypad(makeKeymap(custom_key), row_pins, col_pins, ROW, C
 void setup(void)
 {
 	Serial.begin(9600);
+	pinMode(3, OUTPUT);
 }
 
 void loop(void)
 {
 	char ch = custom_keypad.getKey();
 
-	if (ch) 
-		Serial.println(ch);
+	if (ch == 'A') {
+		digitalWrite(3, HIGH);
+	}
+	if (ch == 'B') {
+		digitalWrite(3, LOW);
+	}
 }
